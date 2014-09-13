3bb0579018d2d4999cea415489df5e22615af93d
Bryan Yap <bryan.yap@alliants.com>
2014-09-13 17:41:00 +0100

    simple validations for Collection

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,6 +2,12 @@ class Collection < ActiveRecord::Base
 
   has_many :posts
 
+  validates :title, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :latitude, presence: true, numericality: true
+  validates :longitude, presence: true, numericality: true
+
   def json_params
     {
       except: [:id, :created_at, :updated_at]
